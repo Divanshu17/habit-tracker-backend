57921345a52871642f698be736802007b884f425
Divanshu17 <divansukachhawacsc@gmail.com>
2025-11-23 12:54:38 +0530

    backend server

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const habitRoutes = require('./routes/habitRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+	origin: [
+		'https://habit-tracker-p.netlify.app',
+		'http://localhost:3000',
+		'http://localhost:5173'
+	],
+	credentials: true,
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+	allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/habits', habitRoutes);
